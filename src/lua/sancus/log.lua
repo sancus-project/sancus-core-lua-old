9a94946cd36467e8da52aae8eae926175ffb5836
Alejandro Mery <amery@geeks.cl>
2011-11-22 16:56:44 +0100

    sancus.log: introduced .logger() constructor

--- src/lua/sancus/log.lua
+++ src/lua/sancus/log.lua
@@ -5,4 +5,21 @@
 
 core = require(... .. ".core")
 
+local setmetatable = setmetatable
+local assert, type = assert, type
+
 module(...)
+
+local _mt = {
+	__index = {},
+}
+
+function _mt.__index:log(str)
+	core.write(9, self.name, str)
+end
+
+function logger(name)
+	assert(name == nil or type(name) == 'string')
+
+	return setmetatable({ name = name }, _mt)
+end
